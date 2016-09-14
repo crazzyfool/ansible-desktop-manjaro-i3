5d09dbef5ba69fc9c8af5300470fc3e0aa25c614
Andy Pike <andy_pikey@hotmail.com>
2016-09-15 00:16:48 +0100

    Conditional checks and improvments

---
+++ files/3-monitor-setup.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+xrandr --output DVI-D-0 --mode 1920x1200 --pos 4480x112 --rotate normal --output HDMI-0 --off --output DVI-I-1 --mode 1920x1200 --pos 0x112 --rotate normal --output DVI-I-0 --off --output DP-1 --primary --mode 2560x1440 --pos 1920x0 --rotate normal --output DP-0 --off
+
